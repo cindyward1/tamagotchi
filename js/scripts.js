b0d56a11be86133d76a2ddd5a4f1e648b4c2580f
Peggy Condon & Cindy Ward <student@epicodus.com>
2014-07-22 10:27:15 -0700

    Tamagotchi behavior is complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,92 @@
 var Tamagotchi = {
-  initialize: function (name, imageNumber) {
+
+  initialize: function (name, imageNumber, birthdate) {
     this.name = name;
     this.image = imageNumber;
-    this.fedLevel = 10;
+    this.birthdate = birthdate;
+    this.isSleeping = false;
+    this.foodLevel = 10;
     this.happinessLevel = 10;
     this.restedLevel = 10;
-    this.healthLevel = 30;
+    this.healthLevel = (this.foodLevel + this.happinessLevel + this.restedLevel) / 3; // calling calcHealthLevel doesn't work here
+  },
+
+  calcHealthLevel: function () {
+    this.healthLevel = (this.foodLevel + this.happinessLevel + this.restedLevel) / 3;
+  },
+
+// Action functions - need buttons
+
+  feed: function () {
+    this.foodLevel++;
+  },
+
+  play: function () {
+    this.happinessLevel++;
+  },
+
+  medicine: function (medicineDose) {
+    this.healthLevel = this.healthLevel + medicineDose;
+  },
+
+  putToBed: function () {
+    if (!this.isSleeping) {
+      this.isSleeping = true;
+    } else {
+      alert("The Tamagotchi is already asleep!");
+    };
+  },
+
+// State variable readers
+
+  isAlive: function () {
+    if ((this.healthLevel < 2) || (this.foodLevel === 0)) {
+      return false;
+    } else {
+      return true;
+    };
+  },
+
+// Warning functions
+
+  restedLevelWarning: function () {
+    var restedWarning = false;
+      if (this.restedLevel < 4) {
+        restedWarning = true;
+        alert("Your Tamagotchi is very tired, you should put it to bed!");
+      };
+    return restedWarning;
+  },
+
+  foodLevelWarning: function () {
+    var foodWarning = false;
+      if (this.foodLevel < 4) {
+        foodWarning = true;
+        alert("Your Tamagotchi is very hungry, you should feed it!");
+      };
+    return foodWarning;
+  },
+
+  happinessLevelWarning: function () {
+    var happinessWarning = false;
+      if (this.happinessLevel < 4) {
+        happinessWarning = true;
+        alert("Your Tamagotchi is very unhappy, you should play with it!");
+      };
+    return happinessWarning;
+  },
+
+// Time passes
+
+  timePasses: function () {
+    if (this.isSleeping) {
+      this.restedLevel++;
+      this.happinessLevel--;
+    } else { // is awake
+      this.restedLevel--;
+      this.happinessLevel--;
+      this.foodLevel--;
+    };
   }
-};
+
+}; // end Tamagotchi
--- spec/specs.js
+++ spec/specs.js
@@ -1,36 +1,140 @@
 describe("Tamagotchi", function() {
-  describe("initialize", function(name, imageNumber) {
+
+  describe("initialize", function(name, imageNumber, birthdate) {
     it("sets the name and a few other properties", function() {
       var myPet = Object.create(Tamagotchi);
-      myPet.initialize("Pet Name",0);
+      myPet.initialize("Pet Name",0,"7/20/2013");
       myPet.image.should.equal(0);
       myPet.name.should.equal("Pet Name");
-      myPet.fedLevel.should.equal(10);
+      myPet.birthdate.should.equal("7/20/2013");
+      myPet.foodLevel.should.equal(10);
       myPet.happinessLevel.should.equal(10);
       myPet.restedLevel.should.equal(10);
-      myPet.healthLevel.should.equal(30);
+      myPet.healthLevel.should.equal(10);
+      myPet.isSleeping.should.equal(false); // start out with Tamagotchi awake
     });
   });
 
-  /* describe("timePasses", function() {
-    it("decreases the amount of food the Tamogatchi has left by 1", function() {
+  describe("calcHealthLevel", function ()  {
+    it("sets the health level to the average of the food level, happiness level and rested level", function () {
       var myPet = Object.create(Tamagotchi);
-      myPet.initialize("lil dragon");
-      myPet.timePasses();
-      myPet.foodLevel.should.equal(9);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.healthLevel.should.equal(10);
+    });
+  });
+
+  describe("feed", function ()   {
+    it("increases the amount of food the Tamagotchi has by 1", function () {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.feed();
+      myPet.foodLevel.should.equal(11);
+    });
+  });
+
+  describe("play", function ()   {
+    it("increases the happiness Level of the Tamagotchi by 1", function () {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.play();
+      myPet.happinessLevel.should.equal(11);
+    });
+  });
+
+  describe("medicine", function ()   {
+    it("increases the health level of the Tamagotchi has by the medicine dosage", function () {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.medicine(5);
+      myPet.healthLevel.should.equal(15);
+    });
+  });
+
+  describe("putToBed", function () {
+    it("changes the Tamagotchi from awake to sleeping", function () {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.putToBed();
+      myPet.isSleeping.should.equal(true);
+    });
+
+    it("gives a warning if the Tamagotchi is already asleep", function () {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.isSleeping = true;
+      myPet.putToBed();
+      myPet.isSleeping.should.equal(true);
     });
   });
 
   describe("isAlive", function() {
-    it("is alive if the food level is above 0", function() {
+    it("is alive if the food level is above 0 or the health level is above 2", function() {
       var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
       myPet.isAlive().should.equal(true);
     });
 
     it("is dead if the food level is 0", function() {
       var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
       myPet.foodLevel = 0;
       myPet.isAlive().should.equal(false);
     });
-  }); */
+
+    it("is dead if the health level is less than 2", function () {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.healthLevel = 1;
+      myPet.isAlive().should.equal(false);
+    });
+  });
+
+  describe("restedLevelWarning", function () {
+    it("should return a warning when rested is less than 4", function () {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.restedLevel = 3;
+      myPet.restedLevelWarning().should.equal(true);
+    });
+  });
+
+  describe("foodLevelWarning", function () {
+    it("should return a warning when rested is less than 4", function () {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.foodLevel = 3;
+      myPet.foodLevelWarning().should.equal(true);
+    });
+  });
+
+  describe("happinessLevelWarning", function () {
+    it("should return a warning when rested is less than 4", function () {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.happinessLevel = 3;
+      myPet.happinessLevelWarning().should.equal(true);
+    });
+  });
+
+  describe("timePasses", function(){
+    it("should decrement the food, happiness, and rested levels if awake", function(){
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.timePasses();
+      myPet.foodLevel.should.equal(9);
+      myPet.happinessLevel.should.equal(9);
+      myPet.restedLevel.should.equal(9);
+    });
+
+    it("should increment the rested level, decrement the happiness level, and do nothing to the food level if sleeping", function(){
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("Pet Name", 0, "7/20/2014");
+      myPet.isSleeping = true;
+      myPet.timePasses();
+      myPet.foodLevel.should.equal(10);
+      myPet.happinessLevel.should.equal(9);
+      myPet.restedLevel.should.equal(11);
+    });
+  });
+
 });
