b85db23f96cd81e5dd4830538a300b1704dec5d3
Peggy Condon & Cindy Ward <student@epicodus.com>
2014-07-22 17:14:42 -0700

    Creates a Tamagotchi and displays all the meters

--- js/scripts.js
+++ js/scripts.js
@@ -99,3 +99,118 @@ var Tamagotchi = {
   }
 
 }; // end Tamagotchi
+
+$(document).ready (function () {
+
+  var buttonSelected = false;
+  var numberImage = 0;
+
+  if (!buttonSelected) {
+    $("button#button0").click (function () {
+      $(".show-message").html("<img class='photo-width' src='./img/char0.png' alt='Picture of first character'>");
+    buttonSelected = true;
+    numberImage = 0;
+    });
+  };
+
+  if (!buttonSelected) {
+    $("button#button1").click (function () {
+      $(".show-message").html("<img class='photo-width' src='./img/char1.png' alt='Picture of first character'>");
+    buttonSelected = true;
+    numberImage = 1;
+    });
+  };
+
+  if (!buttonSelected) {
+    $("button#button2").click (function () {
+      $(".show-message").html("<img class='photo-width' src='./img/char2.png' alt='Picture of first character'>");
+    buttonSelected = true;
+    numberImage = 2;
+    });
+  };
+
+  if (!buttonSelected) {
+    $("button#button3").click (function () {
+      $(".show-message").html("<img class='photo-width' src='./img/char3.png' alt='Picture of first character'>");
+    buttonSelected = true;
+    numberImage = 3;
+    });
+  };
+
+  if (!buttonSelected) {
+    $("button#button4").click (function () {
+      $(".show-message").html("<img class='photo-width' src='./img/char4.png' alt='Picture of first character'>");
+    buttonSelected = true;
+    numberImage = 4;
+    });
+  };
+
+  if (!buttonSelected) {
+    $("button#button5").click (function () {
+      $(".show-message").html("<img class='photo-width' src='./img/char5.png' alt='Picture of first character'>");
+    buttonSelected = true;
+    numberImage = 5;
+    });
+  };
+
+  if (!buttonSelected) {
+    $("button#button6").click (function () {
+      $(".show-message").html("<img class='photo-width' src='./img/char6.png' alt='Picture of first character'>");
+    buttonSelected = true;
+    numberImage = 6;
+    });
+  };
+
+  if (!buttonSelected) {
+   $("button#button7").click (function () {
+      $(".show-message").html("<img class='photo-width' src='./img/char7.png' alt='Picture of first character'>");
+    buttonSelected = true;
+    numberImage = 7;
+    });
+  };
+
+  if (!buttonSelected) {
+    $("button#button8").click (function () {
+      $(".show-message").html("<img class='photo-width' src='./img/char8.png' alt='Picture of first character'>");
+    buttonSelected = true;
+    numberImage = 8;
+    });
+  };
+
+  $("form#form-to-disappear").submit(function(event){
+    event.preventDefault();
+
+    var inputtedName = $("input#tamagotchi-name").val();
+    var inputtedBirthday = $("input#tamagotchi-birthday").val();
+
+    $(".tamagotchi-name").text(inputtedName);
+
+    var formattedDate = new Date(inputtedBirthday);
+    var displayDay = formattedDate.getDate();
+    displayDay++;
+    var displayMonth =  formattedDate.getMonth();
+    displayMonth++;  // JavaScript months are 0-11
+    var displayYear = formattedDate.getFullYear();
+    var dateToDisplay = displayMonth + "/" + displayDay + "/" + displayYear;
+
+    $(".tamagotchi-birthday").text(dateToDisplay);
+
+    $("input#tamagotchi-name").val("");
+    $("input#tamagotchi-birthday").val("");
+
+    $("#form-to-disappear").hide();
+    $("#show-name-birthday").show();
+
+    var myTamagotchi = Object.create(Tamagotchi);
+    myTamagotchi.initialize(inputtedName, numberImage, dateToDisplay);
+
+    $(".food-meter").replaceWith("<meter value=" + myTamagotchi.foodLevel + " min='-1' low='4' high='8' optimum='10' max='15'></meter>");
+    $(".happiness-meter").replaceWith("<meter value=" + myTamagotchi.happinessLevel + " min='-1' low='4' high='8' optimum='10' max='15'></meter>");
+    $(".rested-meter").replaceWith("<meter value=" + myTamagotchi.restedLevel + " min='-1' low='4' high='8' optimum='10' max='15'></meter>");
+    $(".health-meter").replaceWith("<meter value=" + myTamagotchi.healthLevel + " min='-1' low='6' high='8' optimum='10' max='15'></meter>");
+
+    $("#meter-area").show();
+
+  });
+
+});
